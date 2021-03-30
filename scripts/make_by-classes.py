0b43a82d63aab9716fea9207b6a2b7e68583081d
Chaitanya Varier <cvvarier@gmail.com>
2021-03-30 19:17:25 -0400

    sort class histogram by desc freq; grouped stationary/non-vehicular objects into "other"; various wording/grammar/conciseness improvements

--- scripts/make_by-classes.py
+++ scripts/make_by-classes.py
@@ -5,6 +5,9 @@ from firebase_admin.db import Reference
 from functools import reduce
 
 FIREBASE_CRED_FILENAME = "autodash-9dccb-add3cdae62ea.json"
+RELEVANT_CLASSES = ['car', 'bus', 'truck', 'van', 'motorcycle', 'train', 'streetcar', 'pedestrian', 'bicycle', 'animal']
+NOOP_CLASSES = ['NONE', 'test']
+OTHER_CLASS = 'other'
 
 def histogram_by_class(hist, data):
     (key, data) = data
@@ -16,7 +19,11 @@ def histogram_by_class(hist, data):
     for obj in data['bb_fields'].get('objects', []):
         if obj['id'] not in viewed_ids:
             viewed_ids.add(obj['id'])
-            hist[obj['class']] = hist.get(obj['class'], 0) + 1
+            obj_class = obj['class']
+            if obj_class in RELEVANT_CLASSES + NOOP_CLASSES:
+                hist[obj_class] = hist.get(obj_class, 0) + 1
+            else:
+                hist[OTHER_CLASS] = hist.get(OTHER_CLASS, 0) + 1 
     return hist
 
 if __name__ == '__main__':
@@ -35,8 +42,13 @@ if __name__ == '__main__':
     hist = reduce(histogram_by_class, data, {})
     del hist['NONE']
     del hist['test']
-    print(hist)
+    other = hist[OTHER_CLASS]
+    del hist[OTHER_CLASS]
+    items = [ *hist.items() ]
+    items.sort(key=lambda x:x[1], reverse=True)
+    items.append((OTHER_CLASS, other))
+    print(items)
     dtype = [ ('X', object), ('Y', np.uint32) ]
-    Z = np.array([ *hist.items() ], dtype=dtype)
+    Z = np.array(items, dtype=dtype)
     np.savetxt('by-classes.csv', Z, delimiter=',', fmt=['%s', '%d'], header='X,Y', comments='')
 
